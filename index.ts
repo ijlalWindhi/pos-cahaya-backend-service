20f67fec4833dfa55a6e11fd991d45c3072683c9
ijlalWindhi <ijlalwindhi15@gmail.com>
2023-12-05 07:58:42 +0700

    roles route

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { routerUser } from "./routes";
+import { routerUser, routerRole } from "./routes";
 
 dotenv.config();
 
@@ -17,6 +17,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/users", routerUser);
+app.use("/roles", routerRole);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
