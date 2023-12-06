c2cdf9e69a79a1fdac3a6eb1f30deff131bfe37c
ijlalWindhi <ijlalwindhi15@gmail.com>
2023-12-06 07:49:32 +0700

    get detail, register, and update

--- routes/user.ts
+++ routes/user.ts
@@ -4,6 +4,8 @@ import * as userController from "../controller/user";
 const router = express.Router();
 
 router.get("/", userController.getAllUsers);
-router.get("/:uid", userController.getUserDetails);
+router.get("/:uid", userController.getUserDetail);
+router.post("/", userController.createUser);
+router.patch("/:uid", userController.updateUser);
 
 export default router;
