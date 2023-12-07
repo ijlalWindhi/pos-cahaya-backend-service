aac5cf44bf10c9e00eb2f2627963ce2dcf4cdc0b
ijlalWindhi <ijlalwindhi15@gmail.com>
2023-12-07 22:28:11 +0700

    export router categories

--- routes/index.ts
+++ routes/index.ts
@@ -1,5 +1,6 @@
 import routerUser from "./user";
 import routerRole from "./role";
 import routerBusinessUnit from "./business_unit";
+import routerCategory from "./category_product";
 
-export { routerUser, routerRole, routerBusinessUnit };
+export { routerUser, routerRole, routerBusinessUnit, routerCategory };
