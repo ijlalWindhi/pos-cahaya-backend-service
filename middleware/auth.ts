92cbf0abfe1c36ff30e08e785db8eb02b1376124
ijlalWindhi <ijlalwindhi15@gmail.com>
2023-12-07 22:30:02 +0700

    handle error auth

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,10 +1,20 @@
+import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-export const auth = (req: any, res: any, next: any) => {
-  const token = req.header.authorization.split(" ")[1];
+// Extend the Request interface to include a user property
+declare global {
+  namespace Express {
+    interface Request {
+      user?: jwt.JwtPayload;
+    }
+  }
+}
+
+export const auth = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: "No token, authorization denied" });
@@ -17,9 +27,12 @@ export const auth = (req: any, res: any, next: any) => {
 
   try {
     const decoded = jwt.verify(token, secretKey, { algorithms: ["HS256"] });
-    req.user = decoded;
+    req.user = decoded as jwt.JwtPayload;
     next();
   } catch (e) {
+    if (e instanceof jwt.TokenExpiredError) {
+      return res.status(401).json({ message: "Token expired" });
+    }
     res.status(400).json({ message: "Invalid token" });
   }
 };
