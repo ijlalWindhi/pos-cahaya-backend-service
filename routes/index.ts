04f301161719b75fa474ab02e65305124e5e18e1
ijlalWindhi <ijlalwindhi15@gmail.com>
2023-12-07 23:13:14 +0700

    export endpoint employee

--- routes/index.ts
+++ routes/index.ts
@@ -2,5 +2,12 @@ import routerUser from "./user";
 import routerRole from "./role";
 import routerBusinessUnit from "./business_unit";
 import routerCategory from "./category_product";
+import routerEmployee from "./employee";
 
-export { routerUser, routerRole, routerBusinessUnit, routerCategory };
+export {
+  routerUser,
+  routerRole,
+  routerBusinessUnit,
+  routerCategory,
+  routerEmployee,
+};
