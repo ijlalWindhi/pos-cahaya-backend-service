9a1da87382892bddd87d12780303384b194c7549
ijlalWindhi <ijlalwindhi15@gmail.com>
2023-12-11 21:50:49 +0700

    product router

---
+++ routes/product.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import * as productController from "../controller/product";
+import { auth } from "../middleware/auth";
+
+const router = express.Router();
+
+router.get("/", auth, productController.getAllProducts);
+router.get("/:uid", auth, productController.getProductDetail);
+router.get(
+  "/getByBusinessUnit/:uid",
+  auth,
+  productController.getAllProductsByBusinessUnit
+);
+router.post("/", auth, productController.createProduct);
+router.patch("/addStock/:uid", auth, productController.addStockProduct);
+router.patch("/:uid", auth, productController.updateProduct);
+router.delete("/:uid", auth, productController.deleteProduct);
+
+export default router;
